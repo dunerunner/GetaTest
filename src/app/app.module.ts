11550b7eb667be830a1da6c468e126ece3a23cff
dunerunner <powercrush11@gmail.com>
2017-11-30 14:57:01 +0200

    List component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule}    from '@angular/common/http';
 
-
-import { AppComponent } from './app.component';
-
+import {AppComponent} from './app.component';
+import {DataDisplayComponent} from './data-display/data-display.component';
+import {ApiDataService} from './api-data.service';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        DataDisplayComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        NgbModule.forRoot()
+    ],
+    providers: [ApiDataService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
